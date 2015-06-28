8ae9644543d3acb652c7ce0ea2d9857a527d6e1e
Suthep <suthep@gfbd.co.th>
2015-06-28 22:50:31 +0700

    remove files

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="chance/chance.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="underscore/underscore.d.ts" />
