188fc6f2de2e738c26812437c93cf59051f16a1e
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2015-06-29 23:16:58 +0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,28 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var concat = require("gulp-concat");
 var ugly = require("gulp-uglify");
+var htmlmin = require("gulp-minify-html");
 
-gulp.task('default', ["ts"], function () {
+gulp.task('default', ['ts', 'html', 'css'], function () {
+  //
 });
 
-gulp.task("ts", function () {
+gulp.task('ts', function () {
   var tsProject = ts.createProject('tsconfig.json');
   var tsResult = tsProject.src()
     .pipe(ts(tsProject));
 
   return tsResult.js
-    .pipe(concat('all.js'))
-    .pipe(ugly())
+    .pipe(concat('all.js')) 
     .pipe(gulp.dest('dest'));
+});
+
+gulp.task('html', function(){
+  return gulp.src(['app/index.html']) 
+      .pipe(gulp.dest('dest'));
+});
+
+gulp.task('css', function(){
+  return gulp.src(['app/app.css'])
+      .pipe(gulp.dest('dest'));
 });
