da9b48ba7cfc14e30db829e5afbe26a289b655bc
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2015-07-29 14:25:27 +0700

    update initial devices

--- app/controller.ts
+++ app/controller.ts
@@ -10,19 +10,16 @@ module Demo {
     constructor(public $log:angular.ILogService) {
       this.$log.info("starting controller");
 
-      var d1 = new Device('Wireless Speaker', 1, 400, 1, 20);
-      this.myHouse.devices.push(d1);
-
-      var d2 = new Device('Laser Printer', 1, 1600, 1, 10);
-      this.myHouse.devices.push(d2);
+      var d1 = new Device('Refrigerator', 1, 220, 24, 30);
+      this.myHouse.devices.push(d1); 
 
     }
 
     addDevice():void {
-      var name = `${chance.first()}'s wireless speaker`;
+      var name = `${chance.first()}'s server box`;
       var watts = chance.integer({min: 10, max: 150}) * 10;
-      var hoursPerDay = chance.integer({min: 1, max: 20});
-      var daysPerMonth = chance.integer({min: 1, max: 20});
+      var hoursPerDay = chance.integer({min: 1, max: 10});
+      var daysPerMonth = chance.integer({min: 10, max: 20});
       var d = new Device(name, 1, watts, hoursPerDay, daysPerMonth);
       this.myHouse.devices.push(d);
     }
