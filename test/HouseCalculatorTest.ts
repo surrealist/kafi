c52d7b57c2ad05064ee040e39c82f85665e31810
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2015-07-29 18:51:19 +0700

    current house calcualtor test
    just 10 baht per units/month

---
+++ test/HouseCalculatorTest.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import { expect } from 'chai';
+
+describe('HouseCalculator', function() {
+
+	it('should charge 10 baht per unit', function() {
+		expect(10).to.equal(10);
+	});
+
+});
